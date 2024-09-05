32c687dcf278a4d634920191bbf1e3765454d688
Mehdi-Lucas <71726210+Mehdi-Lucas@users.noreply.github.com>
2024-09-06 00:09:25 +0200

    Update pushImages.sh

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+
+# Tag images with GITHUB_SHA
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:${IMAGE_TAG}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:${IMAGE_TAG}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:${IMAGE_TAG}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:${IMAGE_TAG}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:${IMAGE_TAG}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:${IMAGE_TAG}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:${IMAGE_TAG}
+
+# Tag images as latest for convenience
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:${IMAGE_TAG} ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
+
+# Push images with GITHUB_SHA tag
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:${IMAGE_TAG}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:${IMAGE_TAG}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:${IMAGE_TAG}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:${IMAGE_TAG}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:${IMAGE_TAG}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:${IMAGE_TAG}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:${IMAGE_TAG}
+
+# Push images with latest tag
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
@@ -6,4 +35,3 @@ docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
-
